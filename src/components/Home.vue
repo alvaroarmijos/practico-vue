665821966e5306ba1eb2f59e8d7082283a0b9e7a
Alvaro Armijos <alvaro.armijoss@gmail.com>
2022-04-06 23:26:08 -0500

    label y amount en resumen

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,12 @@
       <Header />
     </template>
     <template #resume>
-      <Resume />
+      <Resume
+        :label="label"
+        :date="'06 de Abril 2022'"
+        :total-amount="100000"
+        :amount="amount"
+      />
     </template>
     <template #movements>
       <Movements />
@@ -24,6 +29,12 @@ export default {
     Header,
     Resume,
     Movements,
+  },
+  data() {
+    return {
+      amount: null,
+      label: null,
+    };
   },
 };
 </script>
--- src/components/Resume/Index.vue
+++ src/components/Resume/Index.vue
@@ -1,3 +1,62 @@
 <template>
-  <main>Resume</main>
+  <main>
+    <p>{{ labelVisual }}</p>
+    <h1>{{ amountVisual }}</h1>
+  </main>
 </template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: null,
+    },
+    date: {
+      type: String,
+    },
+    totalAmount: {
+      type: Number,
+    },
+    amount: {
+      type: Number,
+      default: null,
+    },
+  },
+  computed: {
+    amountVisual() {
+      return this.amount !== null ? this.amount : this.totalAmount;
+    },
+    labelVisual() {
+      return this.label !== null ? this.label : this.date;
+    },
+  },
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h1,
+p {
+  margin: 0;
+  text-align: center;
+}
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
+</style>

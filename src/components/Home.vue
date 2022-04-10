eb250fb431df6e0d5f8dd5ea2803c3094c6b2efd
Alvaro Armijos <alvaro.armijoss@gmail.com>
2022-04-10 13:08:39 -0500

    data grafico

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -45,70 +45,98 @@ export default {
     return {
       amount: null,
       label: null,
-      amounts: [100, 200, 500, 200, -400, -600, -300, 0, 300, 500],
       movements: [
         {
           id: 0,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 100,
+          time: new Date("04-01-2022"),
         },
         {
           id: 1,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: -1000,
+          amount: 200,
+          time: new Date("04-01-2022"),
         },
         {
           id: 2,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 500,
+          time: new Date("04-01-2022"),
         },
         {
           id: 3,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: -1000,
+          amount: 200,
+          time: new Date("04-01-2022"),
         },
         {
           id: 4,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: -400,
+          time: new Date("04-01-2022"),
         },
         {
           id: 5,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: -600,
+          time: new Date("04-01-2022"),
         },
         {
           id: 6,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: -300,
+          time: new Date("04-01-2022"),
         },
         {
           id: 7,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 0,
+          time: new Date("04-01-2022"),
         },
         {
           id: 8,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 300,
+          time: new Date("01-01-2022"),
         },
         {
           id: 9,
           title: "Movimiento",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 500,
+          time: new Date("01-01-2022"),
         },
       ],
     };
+  },
+  computed: {
+    amounts() {
+      const lastDays = this.movements
+        .filter((m) => {
+          const today = new Date();
+          const oldDate = today.setDate(today.getDate() - 30);
+
+          return m.time > oldDate;
+        })
+        .map((m) => m.amount);
+
+      return lastDays.map((m, i) => {
+        const lastMovements = lastDays.slice(0, i);
+        return lastMovements.reduce((suma, movement) => {
+          return suma + movement;
+        }, 0);
+      });
+    },
   },
 };
 </script>
